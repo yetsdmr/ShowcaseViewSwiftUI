738638bf1654e46bc12b5e99751a5f7b34d0ed0c
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-06-26 12:26:40 +0300

    Initial Commit

---
+++ ShowcaseViewSwiftUI/ShowcaseViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowcaseViewSwiftUIApp.swift
+//  ShowcaseViewSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShowcaseViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
